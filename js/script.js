69bd64fff903c8942aa531f3684639a420144e03
Ivan Greqov <idgreq@bk.ru>
2022-01-24 00:52:26 +0300

    Add 'Remove book' functionality

--- js/script.js
+++ js/script.js
@@ -8,23 +8,23 @@
       .substring(1);
   const generateId = () => `${s4()}-${s4()}-${s4()}`;
 
-  const myLibrary = [
+  let myLibrary = [
     {
-      id: 0,
+      id: generateId(),
       title: 'Anthem (Barnes & Noble Book Club Edition)',
       author: 'Noah Hawley',
       pages: 100,
       read: false,
     },
     {
-      id: 1,
+      id: generateId(),
       title: 'It Ends with Us',
       author: 'Colleen Hoover',
       pages: 250,
       read: false,
     },
     {
-      id: 2,
+      id: generateId(),
       title: 'To Paradise: A Novel (Signed Book)',
       author: 'Hanya Yanagihara',
       pages: 75,
@@ -40,6 +40,10 @@
     this.read = read;
   }
 
+  Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+  };
+
   function addBookToLibrary(book) {
     // do stuff here
 
@@ -50,22 +54,61 @@
 
   function displayBooks() {
     let rows = '';
-    myLibrary.forEach((book) => {
-      const { id, title, author, pages, read } = book;
-      const bookRow = `
-      <tr data-id="${id}">
-        <td>${title}</td>
-        <td>${author}</td>
-        <td>${pages}</td>
-        <td>${read}</td>
-        <td><button type="button">Remove</button></td>
-      </tr>
-      `;
-      rows += bookRow;
-    });
+    if (myLibrary.length) {
+      myLibrary.forEach((book) => {
+        const { id, title, author, pages, read } = book;
+        const bookRow = `
+        <tr data-id="${id}">
+          <td>${title}</td>
+          <td>${author}</td>
+          <td>${pages}</td>
+          <td>${read}</td>
+          <td>
+            <button class="js-btn js-toggle-read" type="button">Toggle</button>
+            <button class="js-btn js-remove-book" type="button">Remove</button>
+          </td>
+        </tr>
+        `;
+        rows += bookRow;
+      });
+    } else {
+      // TODO: remove message row on adding new book
+      rows += `
+      <tr>
+        <td class="td-message" colspan="5">No books</td>
+      </tr>`;
+    }
 
     booksTable.querySelector('tbody').innerHTML = rows;
   }
+
+  function toggleRead(id) {
+    console.log('Im a toggle read fn', id);
+  }
+
+  function removeBook(id) {
+    // TODO: better remove only one row I guess
+    myLibrary = myLibrary.filter((book) => book.id !== id);
+  }
+
+  // add event listeners
+  booksTable.addEventListener('click', (e) => {
+    const { target } = e;
+
+    if (target.classList.contains('js-btn')) {
+      // TODO: fix selector
+      const id = target.parentNode.parentNode.getAttribute('data-id');
+
+      // NOTE: assume there are only 2 buttons
+      if (target.classList.contains('js-toggle-read')) {
+        toggleRead(id);
+      } else {
+        removeBook(id);
+      }
+
+      displayBooks();
+    }
+  });
 
   // init actions
   displayBooks();
